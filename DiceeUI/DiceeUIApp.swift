df6a3f961a549105d825d81015404f226484dcb0
walruii <walruiix@gmail.com>
2024-11-05 20:42:58 +0530

    Initial Commit

---
+++ DiceeUI/DiceeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeUIApp.swift
+//  DiceeUI
+//
+//  Created by Inderpreet Bhatti on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
