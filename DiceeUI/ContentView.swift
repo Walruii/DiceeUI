1880a1d0e277f8467078b2b78d4a76c62226f438
walruii <walruiix@gmail.com>
2024-11-07 02:06:07 +0530

    add app

--- DiceeUI/ContentView.swift
+++ DiceeUI/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1;
+    @State var rightDiceNumber = 4;
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("GreenBackground")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("DiceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNum: leftDiceNumber)
+                    DiceView(diceNum: rightDiceNumber)
+                }
+                Spacer()
+                Button("Roll")
+                {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }
+                .padding(.horizontal)
+                .font(.system(size: 50, weight: .heavy))
+                .foregroundStyle(.white)
+                .background(.red)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    let diceNum : Int
+    var body: some View {
+        Image("Dice\(diceNum)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
+    }
 }
